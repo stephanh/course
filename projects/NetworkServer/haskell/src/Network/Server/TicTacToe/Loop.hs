60db8bc5320a8dac160a93a275f25166aff357e7
Tony Morris <tmorris@tmorris.net>
2013-08-22 08:26:54 +1000

    network

--- projects/NetworkServer/haskell/src/Network/Server/TicTacToe/Loop.hs
+++ projects/NetworkServer/haskell/src/Network/Server/TicTacToe/Loop.hs
@@ -1,14 +1,16 @@
 module Network.Server.TicTacToe.Loop where
 
-import Prelude hiding (mapM_)
+import Prelude hiding (mapM_, catch)
 import Network.Server.Common.Env
 import System.IO(BufferMode(..))
 import Network(PortID(..), sClose, withSocketsDo, listenOn)
-import Data.IORef(IORef, newIORef)
+import Data.IORef(IORef, newIORef, readIORef)
+import Data.Foldable(Foldable, mapM_)
+import Control.Applicative(Applicative, pure)
 import Control.Monad.Trans(MonadIO(..), MonadTrans(..))
 import Control.Monad(liftM)
 import Control.Concurrent(forkIO)
-import Control.Exception(finally, try, Exception)
+import Control.Exception(finally, try, catch, Exception)
 import Control.Monad(forever)
 
 import Network.Server.Common.Accept
@@ -38,6 +40,13 @@ execLoop ::
 execLoop (Loop l) e =
   fmap fst . l e
 
+initLoop ::
+  Functor f =>
+  (Env v -> f a)
+  -> Loop v s f a
+initLoop f =
+  Loop $ \env s -> fmap (\a -> (a, s)) . f $ env
+
 instance Functor f => Functor (Loop v s f) where
   fmap f (Loop k) =
     Loop (\env -> fmap (\(a, t) -> (f a, t)) . k env)
@@ -60,15 +69,15 @@ instance MonadIO f => MonadIO (Loop v s f) where
 
 etry ::
   Exception e =>
-  (Env v -> s -> IO (a, s))
-  -> IOLoop v s (Either e (a, s))
+  (Env v -> IO a)
+  -> IOLoop v s (Either e a)
 etry k =
-  Loop (\env s -> fmap (\e -> (e, s)) . try . k env $ s)
+  initLoop $ try . k
 
 server ::
   IO w -- server initialise
   -> (w -> IO v) -- client accepted (pre)
-  -> s
+  -> s -- initial state
   -> IOLoop v s () -- per-client
   -> IO a
 server i r t l =
@@ -85,14 +94,109 @@ server i r t l =
        hand s w c `finally` sClose s
 
 perClient ::
-  s
-  -> IOLoop v s x -- client accepted (post)
+  IOLoop v s x -- client accepted (post)
   -> (String -> IOLoop v s a) -- read line from client
   -> IOLoop v s ()
-perClient s q f =
-  let lp = do k <- etry undefined -- lGetLine
+perClient q f =
+  let lp = do k <- etry lGetLine
               case k of Left e -> xprint e
                         Right [] -> lp
                         Right l -> f l >> lp
   in do _ <- q
         lp
+
+loop ::
+  IO w -- server initialise
+  -> (w -> IO v) -- client accepted (pre)
+  -> s -- initial state
+  -> IOLoop v s x -- client accepted (post)
+  -> (String -> IOLoop v s w) -- read line from client
+  -> IO a
+loop i r s q f =
+  server i r s (perClient q f)
+
+iorefServer ::
+  v -- server initialise
+  -> s -- initial state
+  -> IORefLoop v s () -- per-client
+  -> IO a
+iorefServer x s =
+  server (newIORef x) return s
+
+iorefLoop ::
+  v -- server initialise
+  -> s -- initial state
+  -> IORefLoop v s x -- client accepted (post)
+  -> (String -> IORefLoop v s w) -- read line from client
+  -> IO a
+iorefLoop x s q f =
+  iorefServer x s (perClient q f)
+
+pPutStrLn ::
+  String
+  -> IOLoop v s ()
+pPutStrLn s =
+  initLoop (`lPutStrLn` s)
+
+(!) ::
+  Foldable t =>
+  IOLoop v s (t Ref)
+  -> String
+  -> IOLoop v s ()
+clients ! msg =
+ clients >>= purgeClients (\y -> liftIO (lPutStrLn y msg))
+
+infixl 2 !
+
+purgeClients ::
+  Foldable t =>
+  (Ref -> IOLoop s v ())
+  -> t Ref
+  -> IOLoop s v ()
+purgeClients a =
+  mapM_ (\y ->
+    ecatch (a y)
+      (\x -> do _ <- modifyClients (S.delete y)
+                xprint x)
+        )
+
+readEnv ::
+  Applicative f =>
+  Loop v s f (Env v)
+readEnv =
+  initLoop $ pure
+
+readEnvval ::
+  Applicative f =>
+  Loop v s f v
+readEnvval =
+  fmap (envvalL `getL`) readEnv
+
+readIOEnvval ::
+  IORefLoop a s a
+readIOEnvval =
+  initLoop $ \env ->
+    readIORef (envvalL `getL` env)
+
+allClientsButThis ::
+  IOLoop v s (Set Ref)
+allClientsButThis =
+  initLoop $ \env ->
+    fmap (S.delete ((acceptL .@ refL) `getL` env)) (readIORef (clientsL `getL` env))
+
+-- Control.Monad.CatchIO
+ecatch ::
+  Exception e =>
+  IOLoop v s a
+  -> (e -> IOLoop v s a)
+  -> IOLoop v s a
+ecatch (Loop k) f =
+  Loop $ \env s -> k env s `catch` (\e -> let Loop l = f e; o = l env s in l env s)
+
+modifyClients ::
+  (Set Ref -> Set Ref)
+  -> IOLoop v s (Set Ref)
+modifyClients f =
+  initLoop $ \env ->
+    atomicModifyIORef_ (clientsL `getL` env) f
+
